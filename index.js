b11b1a9d37f312a9167441f9c66f28487df3ed38
klimga <136005848+klimga@users.noreply.github.com>
2024-02-09 17:50:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const arreglo = [
+
+    {
+        letra: A,
+        puntaje: 100,
+    }
+    {
+        letra: A,
+        puntaje: 90,
+    }
+    {
+        letra: B, 
+        puntaje: 89,
+    }
+    {
+        letra = B, 
+        puntaje = 80,
+    }
+    {
+        letra: B, 
+        puntaje: 89,
+    }
+    {
+        letra = B, 
+        puntaje = 80,
+    }
+        
+
+]
